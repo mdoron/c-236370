a58ef306b67858fddf8b98353f8005c218a2e614
Doron Meshulam <doronmmm@hotmail.com>
2016-11-16 22:48:57 +0200

    LifeThread -> LifeProducer

--- hw1/src/LifeProducer.java
+++ hw1/src/LifeProducer.java
@@ -1,7 +1,7 @@
 /**
  * Created by raviv on 16/11/2016.
  */
-public class LifeThread extends Thread {
+public class LifeProducer extends Thread {
 	public void run() {
 		System.out.println("Hello from a thread!");
 	}
