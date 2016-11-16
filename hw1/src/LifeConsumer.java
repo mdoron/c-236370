5fc44d04879ea6e98c992c8f9d4c0844e1d65334
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2016-11-17 00:57:08 +0200

    consumer almost done - syncronizing left and checking borders with right way #7

--- hw1/src/LifeConsumer.java
+++ hw1/src/LifeConsumer.java
@@ -1,11 +1,61 @@
+import java.util.Queue;
+
 /**
  * Created by raviv on 16/11/2016.
  */
+/*
+each consumer holds a queue which has
+ */
 public class LifeConsumer extends Thread {
+    Queue<boolean[][]> NeighboursQeueArray[];
+    boolean[][] block;
+    int generations;
+    boolean[][] nextBlock;
 
-        public void run() {
-            System.out.println("Hello from a thread!");
+    public LifeConsumer(Queue<boolean[][]> nqa[], boolean[][] block, int generations) {
+        NeighboursQeueArray = nqa;
+        this.block = block;
+        this.generations = generations;
+    }
+
+
+    public void run() {
+
+        nextBlock = new boolean[block.length][];
+
+        for (int g = 0; g < generations; g++) {
+            for (int i = 0; i < block.length; i++) {
+                for (int j = 0; j < block[i].length; j++) {
+                    int numNeighbors = numNeighbors(i, j, block);
+                    if (numNeighbors == 3 || (block[i][j] && numNeighbors == 2)) {
+                        block[i][j] = true;
+                    }
+                    else {
+                        block[i][j] = false;
+                    }
+                }
+            }
+            boolean[][] tmp;
+            tmp = block;
+            block = nextBlock;
+            nextBlock = tmp;
         }
+    }
 
+
+    private int numNeighbors(int x, int y, boolean[][] field) {
+        int counter = (field[x][y] ? -1 : 0);
+        for (int i = x - 1; i <= x + 1; i++) {
+            if ((i < 0) || (i >= field.length)) {
+                continue;
+            }
+            for (int j = y - 1; j <= y + 1; j++) {
+                if (j < 0 || j >= field[0].length) {
+                    continue;
+                }
+                counter += (field[i][j] ? 1 : 0);
+            }
+        }
+        return counter;
     }
 }
---
+++ hw1/src/Neighbours.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+
+/**
+ * Created by raviv on 16/11/2016.
+ */
+public class Neighbours {
+    LinkedList<Integer> rowList = new LinkedList<>();
+    LinkedList<Integer> colList = new LinkedList<>();
+    LinkedList<Integer> genList = new LinkedList<>();
+}
