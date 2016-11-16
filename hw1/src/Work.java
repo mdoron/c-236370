1cd750444f419992146a26a2e86ba41a0df08f18
Doron Meshulam <doronmmm@hotmail.com>
2016-11-16 23:28:15 +0200

    adding Work

---
+++ hw1/src/Work.java
@@ -0,0 +1,18 @@
+
+public class Work {
+	boolean [][] block;
+	int gen;
+	
+	public Work(boolean[][] block, int gen) {
+		this.block = block;
+		this.gen = gen;
+	}
+	
+	public boolean[][] getBlock() {
+		return this.block;
+	}
+	
+	public int getGen() {
+		return this.gen;
+	}
+}
