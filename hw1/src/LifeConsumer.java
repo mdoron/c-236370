3586a2728a3c1084fe0664fe395498f5c50864bc
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2016-11-16 23:25:00 +0200

    very broken, no clue where to head from here

---
+++ hw1/src/LifeConsumer.java
@@ -0,0 +1,11 @@
+/**
+ * Created by raviv on 16/11/2016.
+ */
+public class LifeConsumer extends Thread {
+
+        public void run() {
+            System.out.println("Hello from a thread!");
+        }
+
+    }
+}
--- hw1/src/ParallelGameOfLife.java
+++ hw1/src/ParallelGameOfLife.java
@@ -1,9 +1,71 @@
 public class ParallelGameOfLife implements GameOfLife {
 
+	/*
+	this should run the simulation game in parallell mode
+	 */
 	public boolean[][][] invoke(boolean[][] initalField, int hSplit, int vSplit,
 			int generations) {
-		// TODO Auto-generated method stub
-		return null;
+		boolean[][][] x=new boolean[2][][];
+		x[0] = get_gen(initalField,hSplit,vSplit, generations-1);
+		x[1] = get_gen(x[0],hSplit,vSplit, 1);
+		return x;
 	}
+
+	public boolean[][] get_gen(boolean[][] initalField, int hSplit, int vSplit, int generations) {
+
+		// init the field -> copy the field to the local array
+		//TODO: parallelize this
+		boolean[][] input=new boolean[initalField.length][];
+		for (int i=0;i<initalField.length;i++){
+			input[i]=new boolean[initalField[0].length];
+			for (int k=0;k<input[i].length;k++){
+				input[i][k]=initalField[i][k];
+			}
+		}
+
+		for(int row=0;row<hSplit;row++) {
+			for(int col = 0;col < vSplit;col++) {
+				new LifeConsumer().start();
+			}
+		}
+
+		boolean[][] result=new boolean[initalField.length][];
+
+		for (int g=0;g<generations;g++){
+			for (int i=0;i<initalField.length;i++){
+				if (result[i]==null){
+					// using first time -> copy the array
+					result[i]=new boolean[initalField[i].length];
+				}
+
+				for (int j=0;j<initalField[i].length;j++){
+					int numNeighbors=numNeighbors(i,j,input);
+					result[i][j]=false;
+					if (numNeighbors==3 || (input[i][j]&& numNeighbors==2)){
+						result[i][j]=true;
+					}
+				}
+			}
+			boolean[][] tmp;
+			tmp=input;
+			input=result;
+			result=tmp;
+		}
+		return input;
+	}
+
+	private int numNeighbors(int x,int y, boolean[][] field ){
+		int counter=(field[x][y]?-1:0);
+		for (int i=x-1; i<=x+1;i++ ){
+			if ((i < 0) || (i >= field.length)){ continue ; }
+			for (int j=y-1; j<=y+1;j++){
+				if (j<0||j>=field[0].length){ continue ; }
+				counter+=(field[i][j]?1:0);
+			}
+		}
+		return counter;
+	}
+
+
 
 }
