89c77cf98ec9a431dda43b26596a17635a388f4d
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2016-11-17 01:38:55 +0200

    This is the consumer almost finished - all thats left to do is the syncronization in numNeighbors for the borders #7 In addition - we realized we dont need a producer

--- hw1/src/LifeConsumer.java
+++ hw1/src/LifeConsumer.java
@@ -5,17 +5,20 @@ import java.util.Queue;
  */
 /*
 each consumer holds a queue which has
+gets the queue of blocks array and the block, the num of generations
  */
 public class LifeConsumer extends Thread {
     Queue<boolean[][]> NeighboursQeueArray[];
-    boolean[][] block;
+    boolean[][] block;    //same with producer
     int generations;
     boolean[][] nextBlock;
+    boolean[][] sendToProd;
 
     public LifeConsumer(Queue<boolean[][]> nqa[], boolean[][] block, int generations) {
         NeighboursQeueArray = nqa;
         this.block = block;
         this.generations = generations;
+        this.sendToProd = sendToProd;
     }
 
 
@@ -25,19 +28,24 @@ public class LifeConsumer extends Thread {
 
         for (int g = 0; g < generations; g++) {
             for (int i = 0; i < block.length; i++) {
+                if (nextBlock[i]==null){
+                    nextBlock[i]=new boolean[block[i].length];
+                }
                 for (int j = 0; j < block[i].length; j++) {
                     int numNeighbors = numNeighbors(i, j, block);
+                    nextBlock[i][j] = false;
                     if (numNeighbors == 3 || (block[i][j] && numNeighbors == 2)) {
-                        block[i][j] = true;
-                    }
-                    else {
-                        block[i][j] = false;
+                        nextBlock[i][j] = true;
                     }
                 }
+            }
+            synchronized (sendToProd) {
+                sendToProd = nextBlock;
             }
             boolean[][] tmp;
             tmp = block;
             block = nextBlock;
+            notify();       //notify producer
             nextBlock = tmp;
         }
     }
@@ -47,10 +55,12 @@ public class LifeConsumer extends Thread {
         int counter = (field[x][y] ? -1 : 0);
         for (int i = x - 1; i <= x + 1; i++) {
             if ((i < 0) || (i >= field.length)) {
+                //TODO: get from producers
                 continue;
             }
             for (int j = y - 1; j <= y + 1; j++) {
                 if (j < 0 || j >= field[0].length) {
+                    //TODO: get from producers
                     continue;
                 }
                 counter += (field[i][j] ? 1 : 0);
