90c305153c403a8faa5c2c126b1c30e792a16b1c
Doron Meshulam <doronmmm@hotmail.com>
2016-11-17 12:37:24 +0200

    Tests structure to #10

--- hw1/src/hw1/ParallelGameOfLife.java
+++ hw1/src/hw1/ParallelGameOfLife.java
@@ -6,6 +6,12 @@ import org.junit.Test;
 
 public class ParallelGameOfLife {
 
+	// setBlock tests
+	// extractBlock tests
+	// calcIndex tests
+	// setNQA tests
+	// get_gen tests
+	// invoke tests
 	@Test
 	public void test() {
 		fail("Not yet implemented");
