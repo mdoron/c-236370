21b24cc009c00f6f81d959e545c09f087011ecef
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2016-11-17 13:26:33 +0200

    only LEFT RIGHT UP DOWN blocks left in checkNeigh #7 nothing is broken

--- hw1/src/LifeConsumer.java
+++ hw1/src/LifeConsumer.java
@@ -15,24 +15,26 @@ public class LifeConsumer extends Thread {
     boolean[][] block;    //same with producer
     int generations;
     boolean[][] nextBlock;
-    int r;
-    int c;
+    //row and col of the block
+    int row;
+    int col;
+    int genNow;
 
     public LifeConsumer(ArrayList<ConcurrentLinkedQueue<Work>> nqa, ConcurrentLinkedQueue<Work> blockHistory, int generations,int row,int col) {
         NeighboursQueueArray = nqa;
         this.blockHistory = blockHistory;
         this.generations = generations;
         this.block = blockHistory.element().getBlock();
-        this.r = row;
-        this.c = col;
+        this.row = row;
+        this.col = col;
     }
 
 
     public void run() {
 
         nextBlock = new boolean[block.length][];
-
-        for (int g = 0; g < generations; g++) {
+        genNow = 1;
+        for (genNow = 1; genNow <= generations; genNow++) {
             for (int i = 0; i < block.length; i++) {
                 if (nextBlock[i]==null){
                     nextBlock[i]=new boolean[block[i].length];
@@ -48,12 +50,47 @@ public class LifeConsumer extends Thread {
             boolean[][] tmp;
             tmp = block;
             block = nextBlock;
-            blockHistory.add(new Work(block,g+1));
+            blockHistory.add(new Work(block,genNow));
             notifyAll();       //notify producer maybe we should do notify instead
             nextBlock = tmp;
         }
     }
 
+
+    public enum DIR {
+        UPLEFT,UP,UPRIGHT,LEFT,RIGHT,DOWNLEFT,DOWN,DOWNRIGHT
+    }
+
+    /*
+    @param d - the direction in which the overflow was in
+    @return - sum of neighbours from the overflowed block which are alive
+     */
+    public int checkNeigh(DIR d,int i,int j) throws InterruptedException {
+        ConcurrentLinkedQueue q = NeighboursQueueArray.get(d.ordinal());
+        if(q == null) return 0;
+        if(q.isEmpty()) {
+            wait();
+        }
+        Work w = (Work) q.poll();
+        if(genNow > w.getGen()) {
+            wait();
+        }
+        //assuming all is fine till here, now we take the neighs from block
+        switch(d) {
+            case UPLEFT:
+                return w.getBlock()[w.getBlock().length][w.getBlock()[0].length]? 1:0;
+
+            case UPRIGHT:
+                return w.getBlock()[w.getBlock().length][0]? 1:0;
+            case DOWNLEFT:
+                return w.getBlock()[0][w.getBlock()[0].length]? 1:0;
+            case DOWNRIGHT:
+                return w.getBlock()[0][0]? 1:0;
+            //TODO: UP RIGHT LEFT DOWN
+            default:
+                return 0;
+        }
+    }
 
     private int numNeighbors(int x, int y, boolean[][] field) {
         int counter = (field[x][y] ? -1 : 0);
@@ -62,12 +99,21 @@ public class LifeConsumer extends Thread {
                 //TODO: get from producers
                 continue;
             }
-            for (int j = y - 1; j <= y + 1; j++) {
-                if (j < 0 || j >= field[0].length) {
-                    //TODO: get from producers
-                    continue;
+            try {
+                for (int j = y - 1; j <= y + 1; j++) {
+                    if (i > 0 && i < field.length && j > 0 && j < field[0].length) counter += (field[i][j] ? 1 : 0);
+                    if (i < 0 && j < 0) counter += checkNeigh(DIR.UPLEFT, i, j);
+                    if (i < 0 && j >= field[0].length) counter += checkNeigh(DIR.UPRIGHT, i, j);
+                    if (i < 0 && j > 0 && j < field[0].length) counter += checkNeigh(DIR.UP, i, j);
+                    if (i >= field.length && j < 0) counter += checkNeigh(DIR.DOWNLEFT, i, j);
+                    if (i >= field.length && j >= field[0].length) counter += checkNeigh(DIR.DOWNRIGHT, i, j);
+                    if (i >= field.length && j > 0 && j < field[0].length) counter += checkNeigh(DIR.DOWN, i, j);
+                    if (i > 0 && i < field.length && j < 0) counter += checkNeigh(DIR.LEFT, i, j);
+                    if (i > 0 && i < field.length && j >= field[0].length) counter += checkNeigh(DIR.RIGHT, i, j);
                 }
-                counter += (field[i][j] ? 1 : 0);
+            }
+            catch(Exception e) {
+                e.printStackTrace();
             }
         }
         return counter;
