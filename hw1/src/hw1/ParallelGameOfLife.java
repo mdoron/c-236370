b0abd443b91df3cb46eccb04af601a8fbce771a2
Doron Meshulam <doronmmm@hotmail.com>
2016-11-17 12:33:54 +0200

    Adding test dir

---
+++ hw1/src/hw1/ParallelGameOfLife.java
@@ -0,0 +1,14 @@
+package hw1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ParallelGameOfLife {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
