4e23fc02c1dec7c3619ae5b7d6c6ec7de70eca89
Doron Meshulam <doronmmm@hotmail.com>
2016-12-06 22:35:22 +0200

    Skeleton

--- hw2/parallel-walsh.c
+++ hw2/parallel-walsh.c
@@ -2,11 +2,34 @@
 #include <stdlib.h>
 #include <omp.h>
 
-void fast_parallel_walsh(int* vec, int vecSize)
-{
+// u = H*v
+
+void fast_parallel_walsh(int* v, int vSize) {
+
 }
 
-void simple_parallel_walsh(int* vec, int vecSize)
-{
+void simple_parallel_walsh(int* v, int vSize) {
+	int u[vSize];
+	int h[vSize];
+	// zerofy(u);
+	// zerofy(h);
+	for(int colNum = 1; colNum <= vSize; colNum) {
+		create_walsh_vector(h, vSize, colNum);
+		u[colNum] = multiply(h, v, vSize);
+	}
 
+	v = u;
+}
+
+// Precondition: length(x) == length(y) == size
+int multiply(int* x, int* y, int size) {
+	int mul = 0;
+	for (int i=0; i<size; i++) {
+		mul += x[i]*y[i];
+	}
+	return mul;
+}
+
+void create_walsh_vector(int* h, int vSize, int colNum) {
+	
 }
