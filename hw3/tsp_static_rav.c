c37d4e97b938743b10ccbbfa76567d32b2da8d4e
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2017-01-15 17:55:22 +0200

    rav

--- hw3/tsp_static_rav.c
+++ hw3/tsp_static_rav.c
@@ -1,18 +1,24 @@
 #include <mpi.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 /*
 @author - Raviv Rachmiel
 @since - 07/01/2017
 */
 
-//normal absolute function as implemented in math.h
-int abs(int a, int b) {
-  return a-b>0? a-b : b-a;
+#define MAX_PATH 10000000
+
+//normal ABSolute function as implemented in math.h
+int ABS(int a) {
+  return a>0? a : a*(-1);
 }
 
 //gets 2 cities and coordinates and citiesNum
 //returns the manhatten distance
 int getDist(int city1,int city2,int *xCoord,int* yCoord,int citiesNum) {
-  return city1==city2? 0 : (abs(xCoord[i]-xCoord[j])+abs(yCoord[i]-yCoord[j]));
+  return city1==city2? 0 : (ABS(xCoord[city1]-xCoord[city2])+ABS(yCoord[city1]-yCoord[city2]));
 }
 
 
@@ -30,7 +36,7 @@ int tsp_main(int citiesNum, int xCoord[], int yCoord[], int shortestPath[]) {
   if (mRank==0){
 		//"master" part - send variables to other processes
 		/*sending*/
-		for(int i=1;i<numberOfProcs;i++){
+		for(int i=1;i<numOfProcs;i++){
 			MPI_Bsend(&citiesNum,1,MPI_INT,i,0,MPI_COMM_WORLD);
 			MPI_Bsend(xCoord,citiesNum,MPI_INT,i,1,MPI_COMM_WORLD);
 			MPI_Bsend(yCoord,citiesNum,MPI_INT,i,2,MPI_COMM_WORLD);
@@ -38,19 +44,19 @@ int tsp_main(int citiesNum, int xCoord[], int yCoord[], int shortestPath[]) {
 	}
 	else{
 		MPI_Recv(&citiesNum,1,MPI_INT,0,0,MPI_COMM_WORLD,&status);
-		xCoord=malloc(sizeof(int)*localCitiesNum);
-		yCoord=malloc(sizeof(int)*localCitiesNum);
+		// xCoord=malloc(sizeof(int)*citiesNum);
+		// yCoord=malloc(sizeof(int)*citiesNum);
 		MPI_Recv(xCoord,citiesNum,MPI_INT,0,1,MPI_COMM_WORLD,&status);
 		MPI_Recv(yCoord,citiesNum,MPI_INT,0,2,MPI_COMM_WORLD,&status);
 	}
 	MPI_Barrier(MPI_COMM_WORLD);
-  int* path = (int*) malloc(sizeof(int)*(citiesNum));
+  //int* path = (int*) malloc(sizeof(int)*(citiesNum));
 
   //using serial algorithm
   if(citiesNum < 6) {
     //for each process other then "master"
-    if(myRank > 0)
-      return INT_MAX;
+    if(mRank > 0)
+      return MAX_PATH;
     int prefix[citiesNum];
     prefix[0] = 0;
     int bestPath[citiesNum];
