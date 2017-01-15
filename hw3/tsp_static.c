bd18ea93702bb3c551941b99697d487bea375908
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2017-01-15 17:56:55 +0200

    rav_keep_working

--- hw3/tsp_static.c
+++ hw3/tsp_static.c
@@ -22,6 +22,57 @@ int getDist(int city1,int city2,int *xCoord,int* yCoord,int citiesNum) {
 }
 
 
+// solves the hamilton path of minimum weight in recursion
+// curInd is the index of last city in the path until now
+// used is a bit array specifying which cities are already in the path
+int recurseSolve(int curInd, int curWeight, int* path, int* used, int* bestPath) {
+	if(curInd == globalCitiesNum - 1) {
+		//adding edge from last city to first city to close the cycle
+		memcpy(bestPath, path, globalCitiesNum * sizeof(int));
+		return curWeight + dists[path[0]][path[globalCitiesNum - 1]];
+	}
+
+	int bestWeight = INT_MAX;
+	int receivedPath[globalCitiesNum];
+	int i;
+	for(i = 0; i < globalCitiesNum; ++i) {
+		if(used[i] == TRUE)
+			continue;
+		// check that the minimum weight that the path would have is not greater than minimum weight found till now.
+		// we check that the weight of the path until now including the next city,
+		//		plus the minimum weight that the left edges would have is lower than minimum weight till now
+		if(curWeight + dists[path[curInd]][i] + minNextEdgesWeight[globalCitiesNum - curInd - 1] >= bestWeight)
+			continue;
+		int ww = curWeight + dists[path[curInd]][i];
+		path[curInd + 1] = i;
+		used[i] = TRUE;
+		int weight = recurseSolve(curInd + 1, ww, path, used, receivedPath);
+		used[i] = FALSE;
+		if(weight < bestWeight) {
+			bestWeight = weight;
+			memcpy(bestPath, receivedPath, globalCitiesNum * sizeof(int));
+		}
+	}
+	return bestWeight;
+}
+
+
+
+
+
+// return the best hamilton path and it's weight, such that it has some prefix of cities.
+// initialWeight is the weight of the prefix
+int solve(int prefix[], int len, int initialWeight, int* bestPath) {
+	int used[globalCitiesNum];
+	memset(used, FALSE, globalCitiesNum * sizeof(int));
+	int path[globalCitiesNum];
+	memcpy(path, prefix, len * sizeof(int));
+	int i;
+	for(i = 0; i < len; ++i)
+		used[prefix[i]] = TRUE;
+	return recurseSolve(len - 1, initialWeight, path, used, bestPath);
+}
+
 // The static parellel algorithm main function.
 /*
 gets the cities num and coordinates
