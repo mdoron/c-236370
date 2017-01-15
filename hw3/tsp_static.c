c4a5c6b8a515e4df1c7a6fbea345dc602f31dc20
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2017-01-15 18:01:16 +0200

    fix bugs serial

--- hw3/tsp_static.c
+++ hw3/tsp_static.c
@@ -25,23 +25,23 @@ int getDist(int city1,int city2,int *xCoord,int* yCoord,int citiesNum) {
 // solves the hamilton path of minimum weight in recursion
 // curInd is the index of last city in the path until now
 // used is a bit array specifying which cities are already in the path
-int recurseSolve(int curInd, int curWeight, int* path, int* used, int* bestPath) {
-	if(curInd == globalCitiesNum - 1) {
+int recurseSolve(int curInd, int curWeight, int* path, int* used, int* bestPath,int citiesNum) {
+	if(curInd == citiesNum - 1) {
 		//adding edge from last city to first city to close the cycle
-		memcpy(bestPath, path, globalCitiesNum * sizeof(int));
-		return curWeight + dists[path[0]][path[globalCitiesNum - 1]];
+		memcpy(bestPath, path, citiesNum * sizeof(int));
+		return curWeight + dists[path[0]][path[citiesNum - 1]];
 	}
 
 	int bestWeight = INT_MAX;
-	int receivedPath[globalCitiesNum];
+	int receivedPath[citiesNum];
 	int i;
-	for(i = 0; i < globalCitiesNum; ++i) {
+	for(i = 0; i < citiesNum; ++i) {
 		if(used[i] == TRUE)
 			continue;
 		// check that the minimum weight that the path would have is not greater than minimum weight found till now.
 		// we check that the weight of the path until now including the next city,
 		//		plus the minimum weight that the left edges would have is lower than minimum weight till now
-		if(curWeight + dists[path[curInd]][i] + minNextEdgesWeight[globalCitiesNum - curInd - 1] >= bestWeight)
+		if(curWeight + dists[path[curInd]][i] + minNextEdgesWeight[citiesNum - curInd - 1] >= bestWeight)
 			continue;
 		int ww = curWeight + dists[path[curInd]][i];
 		path[curInd + 1] = i;
@@ -50,7 +50,7 @@ int recurseSolve(int curInd, int curWeight, int* path, int* used, int* bestPath)
 		used[i] = FALSE;
 		if(weight < bestWeight) {
 			bestWeight = weight;
-			memcpy(bestPath, receivedPath, globalCitiesNum * sizeof(int));
+			memcpy(bestPath, receivedPath, citiesNum * sizeof(int));
 		}
 	}
 	return bestWeight;
@@ -62,15 +62,14 @@ int recurseSolve(int curInd, int curWeight, int* path, int* used, int* bestPath)
 
 // return the best hamilton path and it's weight, such that it has some prefix of cities.
 // initialWeight is the weight of the prefix
-int solve(int prefix[], int len, int initialWeight, int* bestPath) {
-	int used[globalCitiesNum];
-	memset(used, FALSE, globalCitiesNum * sizeof(int));
-	int path[globalCitiesNum];
+int solve(int prefix[], int len, int initialWeight, int* bestPath,int citiesNum) {
+	int used[citiesNum];
+	memset(used, 0, citiesNum * sizeof(int));
+	int path[citiesNum];
 	memcpy(path, prefix, len * sizeof(int));
-	int i;
-	for(i = 0; i < len; ++i)
-		used[prefix[i]] = TRUE;
-	return recurseSolve(len - 1, initialWeight, path, used, bestPath);
+	for(int i = 0; i < len; ++i)
+		used[prefix[i]] = 1;
+	return recurseSolve(len - 1, initialWeight, path, used, bestPath, citiesNum);
 }
 
 // The static parellel algorithm main function.
@@ -111,7 +110,7 @@ int tsp_main(int citiesNum, int xCoord[], int yCoord[], int shortestPath[]) {
     int prefix[citiesNum];
     prefix[0] = 0;
     int bestPath[citiesNum];
-    int minWeight = solve(prefix, 1, 0, bestPath);
+    int minWeight = solve(prefix, 1, 0, bestPath,citiesNum);
     memcpy(shortestPath, bestPath, citiesNum * sizeof(int));
     return minWeight;
   }
