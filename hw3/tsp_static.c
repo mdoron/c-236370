0834a955e0cf59d7bf5d62f87c1143f9756a3518
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2017-01-15 20:28:30 +0200

    adding for doron

--- hw3/tsp_static.c
+++ hw3/tsp_static.c
@@ -154,7 +154,7 @@ int tsp_main(int citiesNum, int xCoord[], int yCoord[], int shortestPath[]) {
 	int path[citiesNum]; // stores the best path with one of the prefixes
 	for(int i = 0; i < size; ++i) {
 		// weight of the prefix
-		int ww = getDist(prefixes[i][0],prefixes[i][1],xCoord,yCoord,citiesNum) + getDist(i,j,xCoord,yCoord,citiesNum);
+		int ww = getDist(prefixes[i][0],prefixes[i][1],xCoord,yCoord,citiesNum) + getDist(prefixes[i][1],prefixes[i][2],xCoord,yCoord,citiesNum);
 		int weight = find(prefixes[i], PREFIX_LENGTH, ww, path,xCoord,yCoord,citiesNum);
 		free(prefixes[i]);
 		if(weight < minWeight) {
